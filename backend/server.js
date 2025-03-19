09c2b594578d5be498d4a3dafb9b5d37195dcdcd
Harsh Dubey <harshdubey06003@gmail.com>
2025-03-19 19:12:40 +0530

    frontendurl

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:3001',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
   })
 );
