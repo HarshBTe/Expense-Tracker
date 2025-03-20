6961a14d9bc5a755f492fbbf277afc7da456d843
Harsh Dubey <harshdubey06003@gmail.com>
2025-03-20 12:55:21 +0530

    eleventh

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { addExpense, getExpenses, deleteExpense } = require('../controllers/expenseController');
 const authMiddleware = require('../middleware/authMiddleware');
 const router = express.Router();
-router.post('/', authMiddleware, addExpense);
-router.get('/', authMiddleware, getExpenses);
+router.post('/', addExpense);
+router.get('/', getExpenses);
 router.delete('/:id', authMiddleware, deleteExpense);
 
 
